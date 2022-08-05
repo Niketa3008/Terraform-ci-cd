2f8f76789a0daf3346d0d33dfecd4f046e254df5
Niketa3008 <83983557+Niketa3008@users.noreply.github.com>
2022-08-05 17:22:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,24 +13,9 @@ resource "google_compute_instance" "firstvm" {
   }
 }
 
-resource "google_compute_instance" "secondvm" {
-  machine_type = var.machine_type
-  name = var.name["name2"]
-  zone = var.zone
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-10"
-    }
-  }
-  network_interface {
-    network = "default"
-  }
-}
 
 output "instance_id" {
   value = google_compute_instance.firstvm.instance_id
 }
-output "instance_id_1" {
-  value = google_compute_instance.secondvm.instance_id
-}
+
 

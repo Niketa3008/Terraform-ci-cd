598911cdbc9a909b2f0552782aa364684eb0e365
Niketa3008 <83983557+Niketa3008@users.noreply.github.com>
2022-08-05 17:19:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,4 @@
 
-#this is to make a resource
-
 resource "google_compute_instance" "firstvm" {
   machine_type = var.machine_type
   name = var.name["name1"]
@@ -15,6 +13,24 @@ resource "google_compute_instance" "firstvm" {
   }
 }
 
+resource "google_compute_instance" "secondvm" {
+  machine_type = var.machine_type
+  name = var.name["name2"]
+  zone = var.zone
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-10"
+    }
+  }
+  network_interface {
+    network = "default"
+  }
+}
+
 output "instance_id" {
   value = google_compute_instance.firstvm.instance_id
 }
+output "instance_id_1" {
+  value = google_compute_instance.secondvm.instance_id
+}
+
